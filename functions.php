e85b2618efe7b7427046dc799b019d00fe9a7540
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-08-26 14:07:41 -0500

    feat: Agregando JavaScript a nuestro tema

--- functions.php
+++ functions.php
@@ -18,6 +18,7 @@ function assets() {
     wp_register_style('montserrat', 'https://fonts.googleapis.com/css?family=Montserrat&display=swap', '', '1.0', 'all');
     wp_enqueue_style('estilos', get_stylesheet_uri(), array('bootstrap', 'montserrat'), '1.0', 'all');
 
+//JavaScripts
     wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', '', '1.16.0', true);
     wp_enqueue_script('boostraps', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array('jquery', 'popper'), '4.4.1', true);
     wp_enqueue_script('custom', get_template_directory_uri() . '/assets/js/custom.js', '', '1.0', true);
